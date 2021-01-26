316fe62f8b63d438546436f8d214b0ca3318f79e
Rod Andrade <rodrigoandradebcc@gmail.com>
2021-01-26 01:27:15 -0300

    Adicionando BG Naruto e alterando cores

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -29,9 +29,9 @@ export default function Footer(props) {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <FooterWrapper {...props}>
-      <a href="https://www.alura.com.br/">
+      {/* <a href="https://www.alura.com.br/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
+      </a> */}
       <p>
         Orgulhosamente criado durante
         {' '}
